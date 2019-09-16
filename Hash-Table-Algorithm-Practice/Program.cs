0df39bed818cabec01cbaa2c7c601198ca8ef983
tdepasquale <t.depasquale@hotmail.com>
2019-09-16 13:49:48 -0400

    Add project files.

---
+++ Hash-Table-Algorithm-Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hash_Table_Algorithm_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
